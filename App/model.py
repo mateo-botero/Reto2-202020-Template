2363d3a1844c51eb381f69911ee4b8cfbed0fd1c
Santiago Felipe Arteaga Martin <safe1985@gmail.com>
2020-09-02 17:26:15 -0500

    agregar esqueleto MVC

---
+++ App/model.py
@@ -0,0 +1,59 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+import config
+from DISClib.ADT import list as lt
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
+assert config
+
+"""
+En este archivo definimos los TADs que vamos a usar,
+es decir contiene los modelos con los datos en memoria
+
+Se define la estructura de un catálogo de libros.
+El catálogo tendrá  una lista para los libros.
+
+Los autores, los tags y los años se guardaran en
+tablas de simbolos.
+"""
+
+# -----------------------------------------------------
+# API del TAD Catalogo de Libros
+# -----------------------------------------------------
+
+
+
+# Funciones para agregar informacion al catalogo
+
+
+
+# ==============================
+# Funciones de consulta
+# ==============================
+
+
+
+# ==============================
+# Funciones de Comparacion
+# ==============================
+
+
---
+++ App/view.py
@@ -0,0 +1,55 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+import sys
+import config
+from DISClib.ADT import list as lt
+from DISClib.DataStructures import listiterator as it
+from App import controller
+assert config
+
+"""
+La vista se encarga de la interacción con el usuario.
+Presenta el menu de opciones  y  por cada seleccion
+hace la solicitud al controlador para ejecutar la
+operación seleccionada.
+"""
+
+# ___________________________________________________
+#  Ruta a los archivos
+# ___________________________________________________
+
+
+
+
+
+# ___________________________________________________
+#  Funciones para imprimir la inforamación de
+#  respuesta.  La vista solo interactua con
+#  el controlador.
+# ___________________________________________________
+
+
+
+# ___________________________________________________
+#  Menu principal
+# ___________________________________________________
